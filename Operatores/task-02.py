91bb7c46151ba3a5be6c5c1be0e0cd7949178d91
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-06-14 00:43:55 +0200

    create Python tasks

---
+++ Operatores/task-02.py
@@ -0,0 +1,53 @@
+# Operatores Tasks
+
+# the difference between = & ==
+  x = 5 # here define a value of x
+  x == 5 # here we ask if x equal the value 5
+
+
+# the meaning of !=
+  x != 5 # that means x not equal the value 5
+
+# create a variable x with value of 10
+  x = 10
+
+# lncreasing a value of x
+  x = 10
+  x += 5
+  x
+  15
+
+# Dividing
+  x = 15
+  x /= 5
+  x
+  3.0
+
+# Multiplying
+  x = 3.0
+  x *= 10
+  x
+  30.0
+
+# Module of x
+  x = 30
+  x %= 3
+  x
+  0
+
+# using python print the module of 11 to 4
+  print(11 % 4)
+  3
+
+# print the exponent of 2,3
+  print(2 ** 3)
+  8
+
+# divide 11 by 3 using python divison
+  11 / 3
+  3.6666666666666665
+
+# can we divide 11 by 3 and get an int number?
+  yes
+  print (11 // 3)
+  3
---
+++ Variables/task-01.py
@@ -0,0 +1,56 @@
+# Variables Tasks
+
+#1 Boolean Variable:
+ x = True
+ 
+#2 an Integer Variable:
+ y = 7
+
+#3 a Float Variable:
+ z = 4.8
+ 
+#4 a String Variable: 
+ s = 'Ahmed'
+ s = "9"
+ 
+#5 convert the int to float:
+ a = 3
+ type(a)
+ <class 'int'>
+ b = float(a)
+ print(b)
+ 3.0
+ type(b)
+ <class 'float'>
+
+#6 convert the str to int:
+# we can convert a str to int, just when a value of str is a number
+ f = '7'
+ type(f)
+ <class 'str'>
+ k = int(f)
+ print(k)
+ 7
+ type(k)
+ <class 'int'>
+
+#7 a list of numbers from 1 to 5 :
+ h = [1, 2, 3, 4, 5]
+
+#8 a tuple of numbers from 10 to 15 :
+ n = (10, 11, 12, 13, 14, 15)
+
+#9 convert a list to tuple:
+ e = [1, 2, 3, 4]
+ s = tuple(e)
+ print(s)
+ (1, 2, 3, 4)
+
+#10 create a dict of 3 values:
+ x = {'name' : 'adam' , 'country' : 'Germany' , 'age' : '32' }
+
+#11 using a semi colon ; in Python to separate multiple
+#statements written on a single line.
+ a = 1 ; b = 2 ; c = 3
+
+#12 Python is an interpreted language.
